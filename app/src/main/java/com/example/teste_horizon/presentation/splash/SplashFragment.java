8333043ad7cf4de9a67cd188f8f3553ae9bf8ec2
gabrielbmmaia <gbmmaia.contato@gmail.com>
2023-03-05 16:19:17 -0300

    feat (gradle) Adicionado blibiotecas do Dagger Hilt

--- app/src/main/java/com/example/teste_horizon/presentation/splash/SplashFragment.java
+++ app/src/main/java/com/example/teste_horizon/presentation/splash/SplashFragment.java
@@ -1,4 +1,4 @@
-package com.example.teste_horizon;
+package com.example.teste_horizon.presentation.splash;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.teste_horizon.R;
 
 public class SplashFragment extends Fragment {
 
